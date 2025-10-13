b1ffaceccc3b9fbf3920e56b85672ffaf2134834
Ari <109524755+Pleushy@users.noreply.github.com>
2025-10-13 22:26:34 +0200

    feat: added main loop and test code

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,6 +1,28 @@
-#include <stdio.h>
+#include "core/renderer.hpp"
+#include "utils/globals.hpp"
+#include <unistd.h>
+#include <queue>
+
+Renderer renderer;
+bool running = true;
+
+void heartbeat(int interval) {
+    int i = 0;
+    while (running) {
+        i++;
+        std::queue<Pixel> pixels{};
+        for (int j = 0; j < 256*64; j++) {
+            pixels.push(Pixel('#', (i+j)%256));
+        }
+        Frame my_frame{256, 64, pixels};
+        renderer.render_frame(my_frame);
+        usleep(interval);
+    }
+}
+
+void ready() { heartbeat(constants::FRAME_INTERVAL); }
 
 int main() {
-    printf("Hello world!\n");
+    ready();
     return 0;
 }
