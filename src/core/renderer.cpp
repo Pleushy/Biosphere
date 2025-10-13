c8c9647cc268934ef09dd9a14a8d60c02ce46bce
Ari <109524755+Pleushy@users.noreply.github.com>
2025-10-13 22:26:59 +0200

    feat: added basic ascii rendering

---
+++ src/core/renderer.cpp
@@ -0,0 +1,32 @@
+#include "core/renderer.hpp"
+#include <cstdio>
+#include <iostream>
+#include <queue>
+#include <sstream>
+#include <unistd.h>
+
+void Renderer::clear_frame() {
+    // Not compatible with every terminal
+    printf("\e[1;1H\e[2J"); 
+}
+
+void Renderer::render_frame(Frame frame) {
+    // Clear the last frame
+    clear_frame();
+
+    // Create a buffer
+    std::stringstream buffer;
+
+    // Add all pixels to buffer
+    for (int i = 0; i < frame.height; i++) {
+        for (int j = 0; j < frame.width; j++) {
+            Pixel current = frame.pixels.front();
+            buffer << "\e[38;5;" << current.color << "m" << current.ch;
+            frame.pixels.pop();
+        }
+        buffer << "\n";
+    }
+
+    // Print buffer
+    std::cout << buffer.str() << std::flush;
+}
