60e6f42c082dc1702a68385da3fb5362e85ec99f
Ari <109524755+Pleushy@users.noreply.github.com>
2025-10-21 23:14:45 +0200

    refactor: changed FPS from 60 to 10, added work-in-progress events

--- include/utils/globals.hpp
+++ include/utils/globals.hpp
@@ -2,6 +2,12 @@
 
 namespace constants {
     constexpr int SECOND_IN_NANOSECONDS = 1000000;
-    constexpr int FRAMES_PER_SECOND = 60;
+    constexpr int FRAMES_PER_SECOND = 10;
     constexpr int FRAME_INTERVAL = SECOND_IN_NANOSECONDS/FRAMES_PER_SECOND;
+    constexpr int DEFAULT_COLOR = 15;
 }
+
+enum Events {
+    INPUT,
+    ENVIRONMENT
+};
