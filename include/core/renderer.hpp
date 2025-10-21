38ffad79494935f62d7b0852ee8d634496631eeb
Ari <109524755+Pleushy@users.noreply.github.com>
2025-10-21 23:13:48 +0200

    refactor: changed rendering system to a queue system

--- include/core/renderer.hpp
+++ include/core/renderer.hpp
@@ -1,10 +1,27 @@
 #pragma once
 
 #include <core/frame.hpp>
+#include <queue>
 
-// Basic renderer class (might get more complicated in the future)
+/** Basic renderer class (might get more complicated in the future)
+*/
 class Renderer {
+    private:
+        // Queue of frames to render
+        std::queue<Frame> render_queue;
+
+        // Clears output
+        void clear(void);
+
+        // Renders frame
+        void render_frame(Frame frame);
     public:
-        void clear_frame(void);     // Clears current frame
-        void render_frame(Frame);   // Renders next frame
+        // Adds a frame into the render queue
+        void add_frame(Frame frame);
+        
+        // Adds multiple frames into the render queue
+        void add_animation(std::queue<Frame> queue);
+
+        // Updates the render queue
+        void update();
 };
--- src/core/renderer.cpp
+++ src/core/renderer.cpp
@@ -6,26 +6,27 @@
 #include <sstream>
 #include <unistd.h>
 
-void Renderer::clear_frame() {
+void Renderer::clear() {
     // Not compatible with every terminal
     printf("\e[1;1H\e[2J");
 }
 
 void Renderer::render_frame(Frame frame) {
     // Make sure the frame is valid
-    if (!is_frame_valid(frame)) {
+    if (!frame::is_valid(frame)) {
         std::cerr << "Invalid frame" << std::endl;
+        return;
     }
 
-    // Clear the last frame
-    clear_frame();
+    // Clear output
+    clear();
 
     // Create a buffer
     std::stringstream buffer;
 
     // Add all pixels to buffer
-    for (int i = 0; i < frame.height; i++) {
-        for (int j = 0; j < frame.width; j++) {
+    for (int i = 0; i < frame.size.y; i++) {
+        for (int j = 0; j < frame.size.x; j++) {
             Pixel current = frame.pixels.front();
             frame.pixels.pop();
             if (!current.rendered) {
@@ -39,4 +40,24 @@ void Renderer::render_frame(Frame frame) {
 
     // Print buffer
     std::cout << buffer.str() << std::flush;
+}
+
+void Renderer::add_frame(Frame frame) {
+    render_queue.push(frame);
+}
+
+void Renderer::add_animation(std::queue<Frame> queue) {
+    while (!queue.empty()) {
+        add_frame(queue.front());
+        queue.pop();
+    }
+}
+
+void Renderer::update() {
+    if (render_queue.empty()) return;
+    
+    Frame next_frame = render_queue.front();
+    render_queue.pop();
+
+    render_frame(next_frame);
 }
